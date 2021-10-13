d2774fd4fce6e37734326aefeca5ed9fbbb73148
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-13 15:20:23 +0200

    adds array with listMail, adds prompt asking the user's email

---
+++ js/mail.js
@@ -0,0 +1,9 @@
+//creo l'array contenente le mail delle persone che possono entrare
+const listMail = ["mario.mario@bros.it", "luigi.mario@bros.it", "wario.mario@bros.it", "waluigi.mario@bros.it"];
+
+//chiedo all'utente la mail
+const mailRichiesta = prompt("Inserisci l'e-mail");
+
+//con un ciclo for controllo l'array e paragono la mail data dall'utente a quelle dentro l'array
+
+    //se corrisponde lo faccio entrare altrimenti resta fuori
